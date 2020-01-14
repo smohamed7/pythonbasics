85f2433415808d2919dcd5d816d4e476bc092a14
smohamed7 <man2suleh@gmail.com>
2020-01-14 07:02:34 -0800

    first commit

---
+++ strings.py
@@ -0,0 +1,27 @@
+"""
+stringis a text anything thatis wrapped with adouble or singkle quote
+"""
+aNumber="10"
+aName="toyota"
+aDrink="fanta"
+
+#getting acharacter from a string_indexing
+x=aName
+print(x)
+
+print(aName[2])
+
+#string slicing getting a range of characters
+newVar="programming"
+slicedChars=newVar[3:7]
+slicedChars=newVar[-4:-8]
+slicedChars=newVar[-8:-4]
+slicedChars=newVar[3:7]
+
+print(slicedChars)
+
+stri="mwai"
+str2="kibaki"
+#concudination is the addition of strings
+full_name=stri +" " + str2
+print(full_name)
---
+++ variables.py
@@ -0,0 +1,9 @@
+#variable is a named storage location
+
+#creating a variable to store my name
+myName="suleiman"
+print(myName)
+
+#crearing a variable to hold a random numer
+myAge=20
+print(myAge)
